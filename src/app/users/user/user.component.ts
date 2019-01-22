2068283dcc9b605ec0306456232552d73e44d887
Paty Alves <patriciaalves10@gmail.com>
2019-01-22 21:26:34 -0200

    using routes

---
+++ src/app/users/user/user.component.ts
@@ -0,0 +1,27 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Params } from '@angular/router';
+
+@Component({
+  selector: 'app-user',
+  templateUrl: './user.component.html',
+  styleUrls: ['./user.component.css']
+})
+export class UserComponent implements OnInit {
+  user: {id: number, name: string};
+
+  constructor(private router:ActivatedRoute) { }
+
+  ngOnInit() {
+    this.user={
+      id: this.router.snapshot.params['id'],
+      name:this.router.snapshot.params['name']
+    }
+
+    this.router.params
+    .subscribe((params:Params)=>{
+      this.user.id=params.id;
+      this.user.name=params.name;
+    });
+  }
+
+}
